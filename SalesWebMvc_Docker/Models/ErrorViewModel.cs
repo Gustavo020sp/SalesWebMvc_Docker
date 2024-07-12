e613fba165014387a11fddbc1edfb82ee1f0d972
Gustavo Gomes <gustavo020sp@gmail.com>
2024-07-12 13:23:01 -0300

    initial git

---
+++ SalesWebMvc_Docker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWebMvc_Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
